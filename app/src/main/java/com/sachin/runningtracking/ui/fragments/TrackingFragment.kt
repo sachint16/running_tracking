ca1f6ef71346ae3f8514f5820093a4eaa96e8eee
Sachin <du.cse.sachin@gmail.com>
2020-12-15 14:47:52 +0530

    cancelling a run

--- app/src/main/java/com/sachin/runningtracking/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/sachin/runningtracking/ui/fragments/TrackingFragment.kt
@@ -2,17 +2,20 @@ package com.sachin.runningtracking.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.sachin.runningtracking.R
 import com.sachin.runningtracking.other.Constants.ACTION_PAUSE_SERVICE
 import com.sachin.runningtracking.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.sachin.runningtracking.other.Constants.ACTION_STOP_SERVICE
 import com.sachin.runningtracking.other.Constants.MAP_ZOOM
 import com.sachin.runningtracking.other.Constants.POLYLINE_COLOR
 import com.sachin.runningtracking.other.Constants.POLYLINE_WIDTH
@@ -35,6 +38,14 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
 
     private var currentTimeInMillis = 0L
+
+
+    private var menu: Menu? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -67,18 +78,63 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
         TrackingService.timeRunInMillis.observe(viewLifecycleOwner, Observer {
             currentTimeInMillis = it
-            val formattedTime = TrackingUtility.getFormattedStopWatchTime(currentTimeInMillis,true)
+            val formattedTime = TrackingUtility.getFormattedStopWatchTime(currentTimeInMillis, true)
             tvTimer.text = formattedTime
         })
     }
 
     private fun toggleRun() {
         if (isTracking) {
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.toolbar_tracking_menu, menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if (currentTimeInMillis > 0) {
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miCancelTracking -> {
+                showCancelTrackingAlertDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingAlertDialog() {
+        val dialog = MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
+                .setTitle("Cancel the Run?")
+                .setMessage("Are you sure to cancel the current run and delete all its data?")
+                .setIcon(R.drawable.ic_delete)
+                .setPositiveButton("Yes") { _, _ ->
+                    stopRun()
+                }
+                .setNegativeButton("No") { dialogInterface, _ ->
+                    dialogInterface.cancel()
+                }
+                .create()
+        dialog.show()
+    }
+
+    private fun stopRun() {
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
+    }
+
 
     private fun moveCameraToUser() {
         if (pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
@@ -98,6 +154,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             btnFinishRun.visibility = View.VISIBLE
         } else {
             btnToggleRun.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
             btnFinishRun.visibility = View.GONE
         }
     }
